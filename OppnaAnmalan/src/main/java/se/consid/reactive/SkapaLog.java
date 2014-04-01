6e503d5496273325873caf17cbdbc2b694f01d63
swanmo <swanmo@gmail.com>
2014-04-01 21:55:47 +0200

    La till meddelande för arende.logskapad

--- OppnaAnmalan/src/main/java/se/consid/reactive/SkapaLog.java
+++ OppnaAnmalan/src/main/java/se/consid/reactive/SkapaLog.java
@@ -17,8 +17,9 @@ public class SkapaLog extends Verticle {
             @Override
             public void handle(final Message<JsonObject> request) {
                 container.logger().info(request.body());
+                final JsonObject body = request.body();
 
-                final JsonObject update = createUpdate(request.body());
+                final JsonObject update = createUpdate(body);
 
                 vertx.eventBus().send("test.mongodb", update, new Handler<Message<JsonObject>>() {
                     @Override
@@ -27,6 +28,8 @@ public class SkapaLog extends Verticle {
                         container.logger().info(answer);
 
                         request.reply(answer);
+
+                        fireEventLogSkapad(body);
                     }
                 });
             }
@@ -38,6 +41,10 @@ public class SkapaLog extends Verticle {
                 container.logger().info("SkapaLog depoly " + result.succeeded());
             }
         });
+    }
+
+    private void fireEventLogSkapad(JsonObject query) {
+        vertx.eventBus().publish("arende.logskapad", query);
     }
 
     protected JsonObject createUpdate(final JsonObject request) {
